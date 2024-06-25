a76728194ae1e6a8681a112342c4122ed0786dda
JackyE97 <zellertln@gmail.com>
2024-06-25 18:16:14 +0300

    стилизовала компоненты

--- vite-project/src/components/Header/Header.jsx
+++ vite-project/src/components/Header/Header.jsx
@@ -1,60 +1,63 @@
+import * as S from "./Header.styled.js";
 import { useState } from "react";
+import PopUser from "../PopUser/PopUser";
+
 
 const Header = ({ setCards, cards }) => {
     const [isOpen, setOpen] = useState(false);
 
-    const handleOpen = () => {
-        setOpen((prev) => !prev )
-    } ;
+    const handleOpen = () => { setOpen((prev) => !prev) };
 
-//     useEffect(() => {
-// }, [isOpen]);
+    const onAddCard = () => {
+        console.log("add card");
 
-const onAddCard = () => {
-    console.log("add card");
+        const newCard = {
+            id: Date.now(),
+            title: "Новая задача",
+            topic: "Без темы",
+            date: "10.05.2024",
+            status: "Без статуса",
+        }
+        const newCardList = [...cards, newCard]
+        setCards(newCardList);
+    };
 
-    const newCard = {
-        id: Date.now(),
-        title: "Новая задача",
-        topic: "Без темы",
-        date: "10.05.2024",
-        status: "Без статуса",
-    }
-    const newCardList = [...cards, newCard]
-    setCards(newCardList);
-};
-
-    return ( 
-        <header className="header">
-        <div className="container">
-            <div className="header__block">
-                <div className="header__logo _show _light">
-                    <a href="" target="_self"><img src="images/logo.png" alt="logo" /></a>
-                </div>
-                <div className="header__logo _dark">
+    return (
+        <S.Header>
+            <S.Container>
+                <S.HeaderBlock>
+                    <S.HeaderLogo>
+                        {/* <a href="" target="_self">
+                        <img src="images/logo.png" alt="logo" />
+                    </a> */}
+                    </S.HeaderLogo>
+                    {/* <div className="header__logo _dark">
                     <a href="" target="_self"><img src="images/logo_dark.png" alt="logo" /></a>
-                </div>
-                <nav className="header__nav">
-                    <button className="header__btn-main-new _hover01" onClick={onAddCard}>
-                        Создать новую задачу</button>
-                    <div className="header__user _hover02" onClick={handleOpen}>
-                        Ivan Ivanov</div>
-                        {isOpen && (
-                                     <div className="header__pop-user-set pop-user-set">
-                                     <p className="pop-user-set__name">Ivan Ivanov</p>
-                                     <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-                                     <div className="pop-user-set__theme">
-                                         <p>Темная тема</p>
-                                         <input type="checkbox" className="checkbox" name="checkbox" />
-                                     </div>
-                                     <button type="button" className="_hover03"><a href="#popExit">Выйти</a></button>
-                                 </div>
-                        )}
-                </nav>
-            </div>
-        </div>
-    </header>
-     );
+                </div> */}
+                    <S.HeaderNav>
+                        <S.HeaderBtnMainNew onClick={onAddCard}>
+                            Создать новую задачу
+                        </S.HeaderBtnMainNew>
+                        <S.HeaderUser onClick={handleOpen}>
+                            Ivan Ivanov</S.HeaderUser>
+                        {isOpen && <PopUser />
+                            // (
+                            //              <div className="header__pop-user-set pop-user-set">
+                            //              <p className="pop-user-set__name">Ivan Ivanov</p>
+                            //              <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
+                            //              <div className="pop-user-set__theme">
+                            //                  <p>Темная тема</p>
+                            //                  <input type="checkbox" className="checkbox" name="checkbox" />
+                            //              </div>
+                            //              <button type="button" className="_hover03"><a href="#popExit">Выйти</a></button>
+                            //          </div>
+                            // )
+                        }
+                    </S.HeaderNav>
+                </S.HeaderBlock>
+            </S.Container>
+        </S.Header>
+    );
 }
- 
+
 export default Header;
---
+++ vite-project/src/lib/topic.js
@@ -0,0 +1,26 @@
+export const topicStyles = {
+    _orange: {
+      backgroundColor: "#FFE4C2",
+      color: "#FF6D00",
+    },
+    _green: {
+      backgroundColor: "#B4FDD1",
+      color: "#06B16E",
+    },
+  
+    _purple: {
+      backgroundColor: "#E9D4FF",
+      color: "#9A48F1",
+    },
+  
+    _gray: {
+      backgroundColor: "#94A6BE",
+      color: "#FFFFFF",
+    },
+  };
+  
+  export const topicHeader = {
+    "Web Design": "_orange",
+    Research: "_green",
+    Copywriting: "_purple",
+  };
