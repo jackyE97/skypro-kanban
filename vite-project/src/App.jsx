d31d6e41009ab3fd5ce1cf0ead3bdcac50535074
JackyE97 <zellertln@gmail.com>
2024-09-14 16:15:59 +0300

    удалила app.css

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,4 @@
-import './App.css';
-import PopNewCard from "./components/PopNewCard/PopNewCard";
-import PopBrowse from "./components/PopBrowse/PopBrowse";
+
 import Header from "./components/Header/Header/";
 import Main from "./components/Main/Main";
 import { useState } from "react";
